4937e192124950a81f46fc79311d10b92a383e26
gjjuarez <george.jose.juarez@gmail.com>
2020-04-06 14:13:24 -0500

    Updated dbcontext

--- mediaplatform/MediaPlatform.Storing/Databases/MediaPlatformDbContext.cs
+++ mediaplatform/MediaPlatform.Storing/Databases/MediaPlatformDbContext.cs
@@ -51,7 +51,15 @@ namespace MediaPlatform.Storing.Databases
       builder.Entity<Video>().HasData(new Video[]
       {
         new Video() {ID = DateTime.Now.Ticks, Title = "Tough Time Never Last, Only Tough People Last", Duration = 31, UploadedBy = "Demi Demi", URL = "https://www.youtube.com/embed/1puR8jGK03A"},
-        new Video() {ID = DateTime.Now.Ticks, Title = "Birds are not real", Duration = 500, UploadedBy = "George"},
+        new Video() {ID = DateTime.Now.Ticks, Title = "Birds are not real", Duration = 247, UploadedBy = "George", URL = "https://www.youtube.com/embed/l30_APBNPXg"},
+        new Video() {ID = DateTime.Now.Ticks, Title = "Americans brace for possible approach of coronavirus peak", Duration = 552, UploadedBy = "Jeremy", URL = "https://www.youtube.com/embed/MwaK-SmcIx0"},
+        new Video() {ID = DateTime.Now.Ticks, Title = "Watch Elon Musk announce the Tesla Cybertruck in 14 minutes", Duration = 1343, UploadedBy = "Jesus", URL = "https://www.youtube.com/embed/464puoD09dM"},
+        new Video() {ID = DateTime.Now.Ticks, Title = "Rick Astley - Never Gonna Give You up (video)", Duration = 332, UploadedBy = "George", URL = "https://www.youtube.com/embed/dQw4w9WgXcQ"},
+        new Video() {ID = DateTime.Now.Ticks, Title = "The Kooks - Naive", Duration = 322, UploadedBy = " George", URL = "https://www.youtube.com/embed/jkaMiaRLgvY"},
+        new Video() {ID = DateTime.Now.Ticks, Title = "Gladiator Opening Scene(Part 1)", Duration = 535, UploadedBy = "Jeremy", URL = "https://www.youtube.com/embed/yXiSp9aJYN4"},
+        new Video() {ID = DateTime.Now.Ticks, Title = "Harry Potter and the Deathly Hallows part 2 - Snape's memories part 2 (HD)", Duration = 428, UploadedBy = "Jesus", URL = "https://www.youtube.com/embed/iqyf-kJWwIg"},
+        new Video() {ID = DateTime.Now.Ticks, Title = "Shaq Tries to Not Make a Face While Eating Spicy Wings | Hot Ones", Duration = 3109, UploadedBy = "George", URL = "https://www.youtube.com/embed/_sZH-psg9yE"},
+        new Video() {ID = DateTime.Now.Ticks, Title = "Engine Placement EXPLAINED", Duration = 1336, UploadedBy = "Jesus", URL = "https://www.youtube.com/embed/SiOSbHo2dvk"},
       });
 
       builder.Entity<Community>().HasData(new Community[]
