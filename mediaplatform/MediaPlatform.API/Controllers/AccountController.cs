e492e9cd8c36b30d31b7e7288da6e267bb5e913d
Jesus <jesusguerecab@gmail.com>
2020-04-05 22:06:23 -0500

    attempt to fix skipped projects

--- mediaplatform/MediaPlatform.API/Controllers/AccountController.cs
+++ mediaplatform/MediaPlatform.API/Controllers/AccountController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MediaPlatform.Domain.Models;
+using MediaPlatform.Storing.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using PizzaBox.Storing.Repositories;
 
 namespace MediaPlatform.API.Controllers
 {
