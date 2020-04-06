91cfd7a22a5610866cf51f35a259c8318a094db5
Jesus <jesusguerecab@gmail.com>
2020-04-06 12:55:16 -0500

    user authentication

--- mediaplatform_client/MediaPlatformClient.MVC/Controllers/AccountController.cs
+++ mediaplatform_client/MediaPlatformClient.MVC/Controllers/AccountController.cs
@@ -9,34 +9,29 @@ using MediaPlatformClient.MVC.Singletons;
 
 namespace mediaplatform_client.MediaPlatformClient.MVC.Controllers
 {
-  public class AccountController : Controller
-  {
-    private static readonly MediaPlatformSingleton _mps = MediaPlatformSingleton.Instance;
-    public const string SessionUsername = "";
-    public IActionResult Login()
-    {
-      return View();
-    }
-
-    [HttpPost]
-    public IActionResult Login(AccountViewModel accountViewModel)
-    {      
-      if (ModelState.IsValid)
+   public class AccountController : Controller
+   {
+      private static readonly MediaPlatformSingleton _mps = MediaPlatformSingleton.Instance;
+      public const string SessionUsername = "";
+      public IActionResult Login()
       {
-        var res = _mps.Client.GetAsync($"http://api/account/{accountViewModel.Username}/{accountViewModel.Password}").GetAwaiter().GetResult();
-        var something = res.Content.ReadAsStringAsync().GetAwaiter().GetResult().ToString();
-        //something should be a string called 'True' or 'False'
-
-        // var name = "";
-        // if (Boolean.TryParse(something))
-        //   name = accountViewModel.Username;
-        // else
-        //   name = "not a bool";
-         
-        HttpContext.Session.SetString(SessionUsername, accountViewModel.Username);
-        return RedirectToAction("Index", "User");
+         return View();
       }
-      return View(accountViewModel);
-    }
-  }
+
+      [HttpPost]
+      public IActionResult Login(AccountViewModel accountViewModel)
+      {
+         if (ModelState.IsValid)
+         {
+            var res = _mps.Client.GetAsync($"http://api/account/{accountViewModel.Username}/{accountViewModel.Password}").GetAwaiter().GetResult();
+            var something = res.Content.ReadAsStringAsync().GetAwaiter().GetResult().ToString();
+            if (something == "true")
+            {
+               HttpContext.Session.SetString(SessionUsername, accountViewModel.Username);
+               return RedirectToAction("Index", "User");
+            }
+         }
+         return View(accountViewModel);
+      }
+   }
 }
