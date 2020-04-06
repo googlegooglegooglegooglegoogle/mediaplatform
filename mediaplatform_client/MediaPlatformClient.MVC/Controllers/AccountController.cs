81decf542850981e665c9e7549a6c54534655e4b
jeremy <jeremychheav@yahoo.com>
2020-04-06 02:43:02 -0500

    added some checks

--- mediaplatform_client/MediaPlatformClient.MVC/Controllers/AccountController.cs
+++ mediaplatform_client/MediaPlatformClient.MVC/Controllers/AccountController.cs
@@ -22,19 +22,18 @@ namespace mediaplatform_client.MediaPlatformClient.MVC.Controllers
       if (ModelState.IsValid)
       {
         var res = _httpClient.GetAsync($"http://api/account/{accountViewModel.Username}/{accountViewModel.Password}").GetAwaiter().GetResult();
-        var something = res.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+        var something = res.Content.ReadAsStringAsync().GetAwaiter().GetResult().ToString();
+        //something should be a string called 'True' or 'False'
 
-        // var name = "";
-        // if (something == null)
-        // {
-        //   name = "the name is null";
-        // }
-        // else
-        // {
-        //   name = "the name is not null";
-        // }
+        var name = "";
+        if (Boolean.TryParse(something)==true)
+          name = something;
+        else if (Boolean.TryParse(something)==false)
+          name = "false";
+        else
+          name = "not a bool";
 
-        HttpContext.Session.SetString(SessionUsername, something.ToString());
+        HttpContext.Session.SetString(SessionUsername, name);
         return RedirectToAction("Index", "User");
       }
       return View(accountViewModel);
